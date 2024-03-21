777f17cb8e8e48dc8688108f3e50138020656987
Boris <BORI0001@e.ntu.edu.sg>
2024-03-21 17:57:53 +0800

    finished home + i htink we should set up a meeting

---
+++ my-app/src/app/components/home/Newly-Added/newly-added.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-newly-added',
+  standalone: true,
+  imports: [],
+  templateUrl: './newly-added.component.html',
+  styleUrl: './newly-added.component.css'
+})
+export class NewlyAddedComponent {
+
+}
